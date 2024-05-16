50b84747201b310e286228b095cb2576272c1c78
Harsh-Vasisht <154325515+Harsh-Vasisht@users.noreply.github.com>
2024-05-16 19:45:26 +0530

    Update ShoppingCart.js

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -97,37 +97,3 @@ cart.displayItems();
 console.log('Total Price:', cart.getTotalPrice());
 
 
-
-class ParkingLot {
-  constructor(size) {
-    this.slots = new Array(size).fill(null);
-    this.availableCount = size;
-  }
-  parkCar(car) {
-    if (this.availableCount === 0) return false;
-    const emptySlot = this.slots.findIndex(slot => slot === null);
-    this.slots[emptySlot] = car;
-    this.availableCount--;
-    return true;
-  }
-  removeCar(car) {
-    const slotIndex = this.slots.findIndex(slot => slot === car);
-    if (slotIndex === -1) return false;
-    this.slots[slotIndex] = null;
-    this.availableCount++;
-    return true;
-  }
-  getAvailableSlots() {
-    return this.availableCount;
-  }
-  isFull() {
-    return this.availableCount === 0;
-  }
-}
-// Example usage
-const lot = new ParkingLot(10);
-lot.parkCar("CAR123");
-lot.parkCar("TRUCK789");
-console.log(lot.getAvailableSlots()); // 8
-console.log(lot.removeCar("TRUCK789")); // true
-console.log(lot.isFull()); // false
